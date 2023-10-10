39c26f70b2bc1bc63d6462b8aafb47a260e64508
chamila9 <chamila9@gmail.com>
2023-10-09 18:42:04 -0700

    Delete modules/configuration directory

--- modules/configuration/templates/confd-sync.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-AWS_REGION=${region}
-confd-sync sync -e ${confd_table_name} \
-                -k ${confd_key_arn} \
-                -p $PWD/config.properties \
-                -s $PWD/secret.properties
